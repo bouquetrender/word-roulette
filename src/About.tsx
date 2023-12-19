048da538d45a3faddd6f4f82279fceeafbfdf6bb
Ve <bouquetrender@gmail.com>
2023-12-19 11:56:32 +0800

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -9,28 +9,11 @@ const About = React.memo((props: Props) => {
     <>
       <div className="text-2xl pt-10 pb-4">
         A self-use webpage for random vocabulary testing, built with TypeScript,
-        React, TailwindCSS, and Vite. You can check out the project's source
-        code on{" "}
-        <a
-          className="underline"
-          href="https://github.com/bouquetrender/word-roulette"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        .
+        React, TailwindCSS, and Vite.
       </div>
 
       <div className="text-2xl pt-4 pb-10">
-        一个用于随机测试单词的自用网页，基于TypeScript、React、TailwindCSS和Vite构建。您可以在{" "}
-        <a
-          className="underline"
-          href="https://github.com/bouquetrender/word-roulette"
-          target="_blank"
-        >
-          GitHub
-        </a>{" "}
-        上查看项目的源代码。
+        一个用于随机测试单词的自用网页，基于TypeScript、React、TailwindCSS和Vite构建。
       </div>
 
       <div className="mt-6 text-2xl flex justify-between">
